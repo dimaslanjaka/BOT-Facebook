33ed7698cbe2e90a8b2a9f2b49a48eb5db78d5be
dimaslanjaka <dimaslanjaka@gmail.com>
2018-11-07 00:08:55 +0700

    Add files via upload

---
+++ autoreaction.php
@@ -0,0 +1,30 @@
+<?php
+///*** FREE OF USE ***///
+require_once('lib/fb.php');
+require('config.php');
+
+$r_male		= 3; // reaction if user MALE , like = 1, love = 2, wow = 3, haha = 4, sad = 7, angry = 8
+$r_female	= 2; // reaction if user FEMALE , like = 1, love = 2, wow = 3, haha = 4, sad = 7, angry = 8
+$max_status	= 4; // Limit Reaction of post
+$token = ''; //Leave blank for Auto Fetch Token
+$useragent = "Mozilla/5.0 (Linux; Android 7.0; Redmi Note 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.80 Mobile Safari/537.36"; // user agent for bot
+$cpu_limit = 50; //50 Percent CPU usage
+//Script tidak akan dijalankan bila server cpu melewati persen diatas
+
+$config['cookie_file'] = 'cookie.txt';
+$config['useragent'] = $useragent;
+if (!file_exists($config['cookie_file'])) {
+    $fp = @fopen($config['cookie_file'], 'w');
+    @fclose($fp);
+}
+foreach (array_filter($auth) as $data){
+  $options["like_comments"] = (isset($data["like_comments"]) && $data["like_comments"] === true ? true : false);
+  $user = (isset($data["user"]) ? $data["user"] : false);
+  echo "Result for ".$user."\n";
+  $pass = (isset($data["pass"]) ? $data["pass"] : false);
+  if (false === $user || $pass === false){ continue; }
+$reaction = new Reaction();
+$user = $reaction->open_ssl("encrypt", $user);
+$pass = $reaction->open_ssl("encrypt", $pass);
+$reaction->React($user, $pass, $token, $r_male, $r_female, $max_status, $options);
+}
---
+++ config.php
@@ -0,0 +1,37 @@
+<?php
+
+///[edit authentication here]
+
+/// /* or // = for comment/escape codes
+
+//--- single account
+$auth = array(
+"user	"	=> 'username',
+"pass" => 'password',
+"token" => "", //blank for auto get token
+"like_comments" => false, //true for autolike comments and replies
+ );
+//--- multiple account
+/*
+$auth = array(
+ array(
+ "user" => "username",
+ "pass" => "password",
+ "token" => "", //blank for auto get token
+ "like_comments" => false, //true for autolike comments and replies
+ ),
+ array(
+ "user" => "username",
+ "pass" => "password",
+ "token" => "", //blank for auto get token
+ "like_comments" => false, //true for autolike comments and replies
+ ),
+ array(
+ "user" => "username",
+ "pass" => "password",
+ "token" => "", //blank for auto get token
+ "like_comments" => false, //true for autolike comments and replies
+ ),
+);
+*/
+?>
